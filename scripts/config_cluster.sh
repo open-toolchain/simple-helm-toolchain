0ad4bbd8bd779479a9d2a66c7524696824d9b121
Philippe Mulet <philippe_mulet@fr.ibm.com>
2017-11-09 15:19:50 +0100

    better msg in scripts

--- scripts/config_cluster.sh
+++ scripts/config_cluster.sh
@@ -20,14 +20,14 @@ cat build.properties
 
 #Check cluster availability
 echo "=========================================================="
-echo "Checking cluster"
+echo "Checking cluster readiness"
 IP_ADDR=$(bx cs workers ${PIPELINE_KUBERNETES_CLUSTER_NAME} | grep normal | awk '{ print $2 }')
 if [ -z ${IP_ADDR} ]; then
   echo -e "${PIPELINE_KUBERNETES_CLUSTER_NAME} not created or workers not ready"
   exit 1
 fi
 
-#Check cluster target namespace 
+echo "Configuring cluster namespace"
 if kubectl get namespace ${CLUSTER_NAMESPACE}; then
   echo -e "Namespace ${CLUSTER_NAMESPACE} found."
 else
@@ -37,9 +37,9 @@ fi
 
 # Grant access to private image registry from namespace $CLUSTER_NAMESPACE
 # reference https://console.bluemix.net/docs/containers/cs_cluster.html#bx_registry_other
-echo -e "Checking access to private image registry from namespace ${CLUSTER_NAMESPACE}"
+echo -e "Configuring access to private image registry from namespace ${CLUSTER_NAMESPACE}"
 IMAGE_PULL_SECRET_NAME="bluemix-toolchain-${PIPELINE_TOOLCHAIN_ID}-${REGISTRY_URL}"
-echo -e "create ${IMAGE_PULL_SECRET_NAME} imagePullSecret if it does not exist"
+echo -e "Checking for presence of ${IMAGE_PULL_SECRET_NAME} imagePullSecret for this toolchain"
 if ! kubectl get secret ${IMAGE_PULL_SECRET_NAME} --namespace ${CLUSTER_NAMESPACE}; then
   echo -e "${IMAGE_PULL_SECRET_NAME} not found in ${CLUSTER_NAMESPACE}, creating it"
   # for Container Registry, docker username is 'token' and email does not matter
@@ -47,6 +47,8 @@ if ! kubectl get secret ${IMAGE_PULL_SECRET_NAME} --namespace ${CLUSTER_NAMESPAC
   echo "enable default serviceaccount to use the pull secret"
   kubectl patch -n ${CLUSTER_NAMESPACE} serviceaccount/default -p '{"imagePullSecrets":[{"name":"'"${IMAGE_PULL_SECRET_NAME}"'"}]}'
   echo -e "Namespace ${CLUSTER_NAMESPACE} is now authorized to pull from the private image registry"
+else
+  echo -e "Namespace ${CLUSTER_NAMESPACE} is already authorized to pull from the private image registry"
 fi
 echo "default serviceAccount:"
 kubectl get serviceAccount default -o yaml
@@ -54,15 +56,15 @@ kubectl get serviceAccount default -o yaml
 echo "TODO -- do not patch the account, rather inject secret into the chart"
 
 echo "=========================================================="
-echo "Checking TILLER enabled (Helm's server component)"
+echo "Configuring TILLER enabled (Helm server-side component)"
 helm init --upgrade
 while true; do
   TILLER_DEPLOYED=$(kubectl --namespace=kube-system get pods | grep tiller | grep Running | grep 1/1 )
   if [[ "${TILLER_DEPLOYED}" != "" ]]; then
-    echo "Tiller ready."
+    echo "Tiller is ready."
     break; 
   fi
-  echo "Waiting for Tiller to be ready."
+  echo "Waiting for Tiller to become ready."
   sleep 1
 done
 helm version
